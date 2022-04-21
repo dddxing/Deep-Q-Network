2cc2c53321a86a85306c45e9dc42d27ac1ad393e
Daming Xing <daming.dx@gmail.com>
2022-04-20 22:46:01 -0400

    first commit

---
+++ q_network.py
@@ -0,0 +1,65 @@
+from dis import dis
+from os import setpgid
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import gym
+
+
+class QNetwork(nn.Module):
+    def __init__(self, env):
+        super(QNetwork, self).__init__()
+        #--------- YOUR CODE HERE --------------#  [q1, q2, qdot1, qdot2, xgoal, ygoal]
+        input_dim = env.observation_space.shape[0]
+        print("input dim: ", input_dim)
+        self.fc1 = nn.Linear(input_dim, 128) 
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 16)
+        self.fc4 = nn.Linear(16, 8)
+
+        self.bn1d = nn.BatchNorm1d(128)
+        self.softmax = nn.Softmax()
+        self.env = env
+        #---------------------------------------
+
+    def forward(self, x, device):
+        #--------- YOUR CODE HERE --------------
+        x = torch.Tensor(x).to(device)
+        # x = torch.from_numpy(x).to(device)
+        x = F.relu(self.bn1d(self.fc1(x)))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        # x = self.softmax(self.fc3(x))
+        return x
+        #---------------------------------------
+        
+    def select_discrete_action(self, obs, device):
+        # Put the observation through the network to estimate q values for all possible discrete actions
+        est_q_vals = self.forward(obs.reshape((1,) + obs.shape), device)
+        # Choose the discrete action with the highest estimated q value
+        discrete_action = torch.argmax(est_q_vals, dim=1).tolist()[0]
+        return discrete_action
+                
+    def action_discrete_to_continuous(self, discrete_action):
+        #--------- YOUR CODE HERE --------------
+        # self.env.step(self.q_network.action_discrete_to_continuous(discrete_action))
+        # print("discrete_action: ", discrete_action)
+        step = 0.3
+        dict_ = {
+            0: np.array([-step, 0]),
+            1: np.array([step, 0]),
+            2: np.array([0, -step]),
+            3: np.array([0, step]),
+            4: np.array([step, step]),
+            5: np.array([step, -step]),
+            6: np.array([-step, step]),
+            7: np.array([0, 0]),
+            }
+        #---------------------------------------
+        continous_action = dict_[discrete_action]
+        return continous_action
+
+if __name__ == "__main__":
+    pass
---
+++ train_dqn.py
@@ -0,0 +1,194 @@
+import argparse
+
+import numpy as np
+import time
+import random
+import os
+import signal
+from contextlib import contextmanager
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import time
+import pandas as pd
+from replay_buffer import ReplayBuffer
+from q_network import QNetwork
+from arm_env import ArmEnv
+
+
+#---------- Utils for setting time constraints -----#
+class TimeoutException(Exception): pass
+
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException("Timed out!")
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+#---------- End of timing utils -----------#
+
+reward_collection = []
+
+class TrainDQN:
+
+    @staticmethod
+    def add_arguments(parser):
+        # Common arguments
+        parser.add_argument('--learning_rate', type=float, default=7e-4,
+                            help='the learning rate of the optimizer')
+        # LEAVE AS DEFAULT THE SEED YOU WANT TO BE GRADED WITH
+        parser.add_argument('--seed', type=int, default=1,
+                            help='seed of the experiment')
+        parser.add_argument('--save_dir', type=str, default='models',
+                            help="the root folder for saving the checkpoints")
+        parser.add_argument('--gui', action='store_true', default=False,
+                            help="whether to turn on GUI or not")
+        # 7 minutes by default
+        parser.add_argument('--time_limit', type=int, default=7*60,
+                            help='time limits for running the training in seconds')
+
+    def __init__(self, env, device, args):
+        random.seed(args.seed)
+        np.random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        torch.backends.cudnn.deterministic = False
+        self.env = env
+        self.env.seed(args.seed)
+        self.env.observation_space.seed(args.seed)
+        self.device = device
+        self.q_network = QNetwork(env).to(self.device)
+        self.t_network = QNetwork(env).to(self.device)
+        print(self.device.__repr__())
+        print(self.q_network)
+
+    def save_model(self, episode_num, episode_reward, args):
+        model_folder_name = f'episode_{episode_num:06d}_reward_{round(episode_reward):03d}'
+        if not os.path.exists(os.path.join(args.save_dir, model_folder_name)):
+            os.makedirs(os.path.join(args.save_dir, model_folder_name))
+        torch.save(self.q_network.state_dict(), os.path.join(args.save_dir, model_folder_name, 'q_network.pth'))
+        print(f'model saved to {os.path.join(args.save_dir, model_folder_name, "q_network.pth")}\n')
+                
+    def train(self, args):
+
+        #--------- YOUR CODE HERE --------------
+        
+        
+        lr = 0.001#args.learning_rate
+        optimizer = optim.Adam(self.q_network.parameters(), lr=lr)
+        
+        loss_fcn = nn.MSELoss()
+        N = 10000
+        replay_buffer = ReplayBuffer(N)
+
+
+        # target_network.load_state_dict(main_network.state_dict())
+        # self.q_network.eval()
+
+        gamma = 0.9
+        num_episodes = 800
+        batch_size = 32
+        epsilon = 0.1
+        num_steps = 200
+        update_steps = 10
+        self.q_network.eval()
+        self.t_network.eval()
+        for i_episode in range(1, num_episodes+1):
+            
+            state  = self.env.reset()
+            # state = self.env.arm.get_state()
+            episode_reward = 0
+            # print("episode, ", i_episode)
+        
+            for step in range(1, num_steps+1):
+                # With probability ε, at = random
+                if np.random.random() < epsilon:
+                    # print("Exploring..")
+                    discrete_action = np.random.randint(0, 7)
+
+                # otherwise at = maxaQA(s, a)
+                else:
+
+                    discrete_action = self.q_network.select_discrete_action(torch.from_numpy(state).float(), self.device)
+
+                # discrete to continuous
+                continuous_action = self.q_network.action_discrete_to_continuous(discrete_action)
+                
+                # Execute action
+                next_state, reward, done, _ = self.env.step(continuous_action)
+
+                if done or next_state is None:
+                    break
+
+                # update reward 
+                episode_reward += reward
+                reward_collection.append(episode_reward)
+                # store transition into replay memory
+                replay_buffer.put((state, discrete_action, reward, next_state, done)) # not sure if action should be disc or cont?
+            
+                # accumulate some steps to a batch
+                if step > batch_size:
+
+                    # train network
+                    states, actions, rewards, next_states, dones = replay_buffer.sample(batch_size)
+
+                    states = torch.FloatTensor(states)
+                    actions = torch.FloatTensor(actions)
+                    rewards = torch.FloatTensor(rewards)
+                    next_states = torch.FloatTensor(next_states)
+                    dones = torch.FloatTensor(dones)
+                    
+                    # current_q
+                    current_q_values = self.q_network(states, self.device).gather(1, actions.view(-1, 1).type(torch.int64)).squeeze()
+                    max_next_q_values, _ = torch.max(self.t_network(next_states, self.device), dim=1)
+                    expected_q_values = rewards + (gamma * max_next_q_values)
+                    # print("current_q_values size ", current_q_values.size())
+                    # print("expected_q_values size ", expected_q_values.size())
+                    loss = loss_fcn(current_q_values, expected_q_values)
+                    
+                    optimizer.zero_grad()
+                    loss.backward()
+                    
+                    optimizer.step()
+
+                if step % update_steps == 0:
+                    self.t_network.load_state_dict(self.q_network.state_dict())
+
+                state = next_state
+
+            print(f"episode={i_episode}, global_step: {step*(i_episode)}, episode_reward: {episode_reward} ")
+            self.save_model(i_episode, episode_reward, args)
+            #---------------------------------------        
+
+def moving_average(x, w):
+    return np.convolve(x, np.ones(w), 'valid') / w
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    TrainDQN.add_arguments(parser)
+    args = parser.parse_args()
+    args.timestr = time.strftime("%Y-%m-%d_%H-%M-%S")
+    args.save_dir = os.path.join(args.save_dir, args.timestr)
+    if not args.seed: args.seed = int(time.time())
+
+    env = ArmEnv(args)
+    device = torch.device('cpu')
+    # declare Q function network and set seed
+    tdqn = TrainDQN(env, device, args)
+    # run training under time limit
+    try:
+        with time_limit(args.time_limit):
+            tdqn.train(args)
+    except TimeoutException as e:
+        print("You ran out of time and your training is stopped!")
+
+    mv_avg = moving_average(reward_collection, 10)
+
+    plt.plot(reward_collection, "r--")
+    plt.plot(mv_avg, "b-")
+    plt.show()
+    
